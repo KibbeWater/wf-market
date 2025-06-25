7d4c321f71d0e700dddf82efc960766ed7356dc1
Kenya-DK <kenni.k@hotmail.com>
2025-06-25 16:01:12 +0200

    Removes HTTP client construction
    Removes the HTTP client construction function.
    
    This commit removes the function responsible for building a `reqwest::Client` with default headers.
    The functionality related to building the rate limiter is retained.

--- src/utils.rs
+++ src/utils.rs
@@ -7,29 +7,6 @@ use governor::{
 };
 
 /**
-INTERNAL: Build the HTTP client with default settings
-
-# Arguments
-- `auth`: Authentication token used when communicating with authenticated endpoints
-
-# Returns
-- A `reqwest::Client` with assigned default headers
-*/
-pub(super) fn build_http(auth: Option<String>) -> reqwest::Client {
-    let mut headers = reqwest::header::HeaderMap::new();
-    if let Some(auth) = auth {
-        headers.insert(reqwest::header::AUTHORIZATION, auth.parse().unwrap());
-    }
-    headers.insert("language", "en".parse().unwrap());
-    headers.insert("platform", "pc".parse().unwrap());
-
-    reqwest::Client::builder()
-        .default_headers(headers)
-        .build()
-        .unwrap()
-}
-
-/**
 INTERNAL: Build the rate limiter for throttling outgoing requests to max allowed speeds
 */
 pub(super) fn build_limiter(rps: NonZeroU32) -> RateLimiter<NotKeyed, InMemoryState, DefaultClock> {
